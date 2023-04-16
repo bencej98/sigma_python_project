95fb257ba723e4c6cc127f16038923a132562b36
bencejenei <bencej9812@gmail.com>
2023-04-16 18:44:28 +0200

    Change description check to different method

--- marvel.py
+++ marvel.py
@@ -2,18 +2,26 @@ import json
 import requests
 import constants
 
-marvel_api_key = constants.marvel_api_key
 
-while True:
-    character = input("Enter a character name: ")
-    if character == "":
-        print("Please provide a valid character name")
-        continue
-    else:
-        break
+def main():
+    character = get_character()
+    character_information = get_character_information(character)
+    if character_information is not None:
+        check_character_information(character_information)
 
+
+def get_character():
+    while True:
+        character = input("Enter a character name: ").strip()
+        if character == "":
+            print("Please provide a valid character name")
+            continue
+        else:
+            return character
+        
 
 def get_character_information(character: str) -> json:
+    marvel_api_key = constants.marvel_api_key
     api_url = f"https://gateway.marvel.com:443/v1/public/characters?name={character}&ts=1&apikey={marvel_api_key}"
     try:
         response = requests.get(api_url)
@@ -30,16 +38,14 @@ def check_character_information(information: json) -> str:
         for details in information:
             name = details["name"]
             description = details["description"]
-            if description == "":
+            if not description:
                 print("No description found")
                 return 0
             print(f"Name: {name}")
             print(f"Description: {description}")
-    elif character_information is None:
-        return 0
     else:
         print("No data found or incorrect name entered")
 
 
-character_information = get_character_information(character)
-check_character_information(character_information)
+if __name__ == "__main__":
+    main()
